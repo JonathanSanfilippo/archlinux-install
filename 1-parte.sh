c1e80f2c0969fb088b27faef94ef9b3b1ecac711
Jonathan Sanfilippo <103053714+JonathanSanfilippo@users.noreply.github.com>
2023-02-15 19:02:39 +0000

    Add files via upload

--- 1-parte.sh
+++ 1-parte.sh
@@ -6,41 +6,41 @@
 #p1="/dev/nvme0n1p1"
 #p2="/dev/nvme0n1p2"
 #p3="/dev/nvme0n1p3"
-#p4="/dev/nvme0n1p4"
-#p5="/dev/nvme0n1p5"
 
-#ssd
-p1="/dev/sda1"
-p2="/dev/sda2"
-p3="/dev/sda3"
-p4="/dev/sda4"
-p5="/dev/sda5"
+#vda
+#p1="/dev/vda1"
+#p2="/dev/vda2"
+#p3="/dev/vda3"
 
-c="gb,nl"
+#sda
+#p1="/dev/sda1"
+#p2="/dev/sda2"
+#p3="/dev/sda3" 
+
+
+country="gb"
 editor="vim"
-#editor="nano"
 kernel="linux"
 
 
 
 #Formattazione delle partizioni
 mkfs.fat -F32 $p1
-mkswap  $p2
-mkfs.btrfs -f $p3      
+mkfs.btrfs -f $p2      
 
 #Montaggio e sottovolumi
-mount $p3 /mnt           
+mount $p2 /mnt           
 btrfs su cr /mnt/@              
 btrfs su cr /mnt/@home      
 umount /mnt                             
-mount -o noatime,commit=120,compress=zstd,space_cache=v2,ssd,subvol=@ $p3 /mnt 
+mount -o noatime,commit=120,compress=zstd,space_cache=v2,ssd,subvol=@ $p2 /mnt 
 mkdir -p /mnt/{boot,home,iso,scripts} 
-mount -o noatime,commit=120,compress=zstd,space_cache=v2,ssd,subvol=@home $p3 /mnt/home 
+mount -o noatime,commit=120,compress=zstd,space_cache=v2,ssd,subvol=@home $p2 /mnt/home 
 mount $p1 /mnt/boot 
-swapon
 
 
-reflector --verbose -c $c -a 6  --sort rate --save /etc/pacman.d/mirrorlist
+
+reflector --verbose -c $country --sort rate --save /etc/pacman.d/mirrorlist
 pacstrap /mnt base base-devel $kernel linux-firmware $editor
 genfstab -Up /mnt > /mnt/etc/fstab
 cp 2-parte.sh /mnt/home/
--- 2-parte.sh
+++ 2-parte.sh
@@ -2,28 +2,22 @@
 # author Jonathan Sanfilippo
 # installazione di recovery parte seconda chroot
 
-#nvme
-#p1="/dev/nvme0n1p1"
-#p2="/dev/nvme0n1p2"
-#p3="/dev/nvme0n1p3"
-#p4="/dev/nvme0n1p4"
-#p5="/dev/nvme0n1p5"
 
-#ssd
-p1="/dev/sda1"
-p2="/dev/sda2"
-p3="/dev/sda3"
-p4="/dev/sda4"
-p5="/dev/sda5"
 
 localhost="archlinux"
-user="user"
+user="username"
 rootpw="password"
 userpw="password"
 localegen="en_US.UTF-8 UTF-8"
 localeconf="LANG=en_US.UTF-8"
 km="us"
 localtime="Europe/London"
+p="sda2"
+#p="vda2"
+#p="nvme0n1p2"
+
+
+
 
 ln -sf /usr/share/zoneinfo/$localtime /etc/localtime 
 hwclock --systohc
@@ -42,6 +36,7 @@ usermod -aG video $user
 echo "$user ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/$user
 echo "$user ALL=NOPASSWD: /usr/bin/pacman" >> /etc/sudoers.d/$user
 echo "$user ALL=NOPASSWD: /usr/bin/pikaur" >> /etc/sudoers.d/$user
+echo "$user ALL=NOPASSWD: /usr/bin/vim" >> /etc/sudoers.d/$user
 
 
 
@@ -53,21 +48,15 @@ echo "timeout 3" >> /boot/loader/loader.conf
 echo "title Arch Linux
 linux /vmlinuz-linux
 initrd /initramfs-linux.img
-options root=$p3 rootflags=subvol=@ rw quiet loglevel=3 rd.system.show_status=auto rd.udev.log_level=3" > /boot/loader/entries/arch.conf
-
-#echo "title Recovery ISO
-#linux /live/vmlinuz-linux
-#initrd /live/initramfs-linux.img
-#options img_dev=$p4 img_loop=archlinux.iso copytoram rw" > /boot/loader/entries/live.conf
-#mkinitcpio -P
+options root=/dev/$p rootflags=subvol=@ rw quiet loglevel=3 rd.system.show_status=auto rd.udev.log_level=3" > /boot/loader/entries/arch.conf
 
 
-#GNOME
-# pacman -S gnome xorg wpa_supplicant wireless_tools netctl net-tools iw networkmanager alsa-utils pipewire-pulse firewalld mtools dosfstools gparted bluez bluez-utils reflector acpi cronie pacman-contrib git wget jq gparted 
-# systemctl enable gdm
+#GNOME Minimal gnome installation
+ pacman -S wpa_supplicant wireless_tools netctl net-tools iw networkmanager alsa-utils pipewire-pulse firewalld mtools dosfstools gparted reflector acpi cronie git gnome-tweaks gnome-shell nautilus gnome-console gvfs gnome-control-center xdg-user-dirs-gtk gdm xorg evince eog gnome-text-editor gnome-keyring
+ systemctl enable gdm
 
 #Cinnamon
-# pacman -S cinnamon nemo-fileroller gnome-terminal lightdm-gtk-greeter lightdm-gtk-greeter-settings xdg-user-dirs-gtk xorg wpa_supplicant wireless_tools netctl net-tools iw networkmanager alsa-utils pipewire-pulse firewalld mtools dosfstools gparted bluez bluez-utils reflector acpi cronie pacman-contrib git wget jq gparted  
+# pacman -S cinnamon nemo-fileroller gnome-terminal lightdm-gtk-greeter lightdm-gtk-greeter-settings xdg-user-dirs-gtk xorg wpa_supplicant wireless_tools netctl net-tools iw networkmanager alsa-utils pipewire-pulse firewalld mtools dosfstools gparted bluez bluez-utils reflector acpi cronie git xed evince eog gnome-calculator gnome-clocks gnome-system-monitor gparted  
 # systemctl enable lightdm
 
 #XFCE
@@ -75,7 +64,7 @@ options root=$p3 rootflags=subvol=@ rw quiet loglevel=3 rd.system.show_status=au
 # systemctl enable lightdm
 
 #KDE
-# pacman -S sddm plasma ark konsole dolphin xorg wpa_supplicant wireless_tools netctl net-tools iw networkmanager alsa-utils pipewire-pulse firewalld mtools dosfstools gparted bluez bluez-utils reflector acpi cronie pacman-contrib git wget jq gparted 
+# pacman -S sddm plasma ark konsole dolphin xorg wpa_supplicant wireless_tools netctl net-tools iw networkmanager alsa-utils  firewalld mtools dosfstools gparted bluez bluez-utils reflector acpi cronie pacman-contrib git wget jq gparted 
 # systemctl enable sddm  
 
 #servizi
@@ -83,7 +72,7 @@ systemctl enable NetworkManager
 systemctl enable firewalld
 #systemctl enable bluetooth
 systemctl enable cronie
-systemctl enable reflector.timer
+#systemctl enable reflector.timer
 
 
 rm -r /home/2-parte.sh
